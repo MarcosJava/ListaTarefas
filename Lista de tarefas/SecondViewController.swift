5323c6cea7ce251abe1eaa40e8b306708a785431
Marcos Felipe Souza <mfelipesp@gmail.com>
2015-12-15 20:45:25 -0200

    Initial Commit

---
+++ Lista de tarefas/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Lista de tarefas
+//
+//  Created by Marcos Felipe Souza on 15/12/15.
+//  Copyright © 2015 Marcos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
